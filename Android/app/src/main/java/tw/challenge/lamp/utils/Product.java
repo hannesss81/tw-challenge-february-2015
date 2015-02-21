40e3feb46debbe8520d68c979d1095b79ea5c91b
Hans <soulyyy@gmail.com>
2015-02-21 16:47:49 +0200

    merge

--- Android/app/src/main/java/tw/challenge/lamp/utils/Product.java
+++ Android/app/src/main/java/tw/challenge/lamp/utils/Product.java
@@ -4,4 +4,38 @@ package tw.challenge.lamp.utils;
  * Created by Hans on 21.02.2015.
  */
 public class Product {
+
+    String name;
+    double price;
+    String barCode;
+    int count = 1;
+
+    public Product(String name, double price, String barCode, int count) {
+        this(name, price, barCode);
+        this.count = count;
+    }
+
+    public Product(String name, double price, String barCode) {
+        this.name = name;
+        this.price = price;
+        this.barCode = barCode;
+        this.count = 1;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (!barCode.equals(product.barCode)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return barCode.hashCode();
+    }
 }
