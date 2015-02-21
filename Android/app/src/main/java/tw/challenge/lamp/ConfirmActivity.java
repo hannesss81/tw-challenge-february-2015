9e7789bd445bd9cf1ac93ad78fa2ca5f8d93907a
Marek Pagel <pagel.marek@gmail.com>
2015-02-21 17:14:25 +0200

    commit

---
+++ Android/app/src/main/java/tw/challenge/lamp/ConfirmActivity.java
@@ -0,0 +1,10 @@
+package tw.challenge.lamp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConfirmActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        
+}
