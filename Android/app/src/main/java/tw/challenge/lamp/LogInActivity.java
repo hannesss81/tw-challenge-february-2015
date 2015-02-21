f9d538ccffb709bd6d5ac19c4e0d3ad301e20105
Joonas Lõmps <lomps.joonas@hotmail.com>
2015-02-21 15:24:22 +0200

    LoginActivity

---
+++ Android/app/src/main/java/tw/challenge/lamp/LogInActivity.java
@@ -0,0 +1,48 @@
+package tw.challenge.lamp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class LogInActivity extends Activity {
+
+    final List<String> usernames = Arrays.asList("Test", "TransferWise", "CrtScnnr");
+    final List<String> passwords = Arrays.asList("qwerty", "qwerty", "qwerty");
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.login_activity);
+
+
+        Button button = (Button) findViewById(R.id.loginBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = ((EditText) findViewById(R.id.username)).getText().toString();
+                String password = ((EditText) findViewById(R.id.password)).getText().toString();
+                Context context = getApplicationContext();
+                if(passwords.get(usernames.indexOf(username)) == password) {
+                    //successful login
+                    Intent intent = new Intent(context, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    //failed...
+
+                    Toast.makeText(context, "Invalid username or password", Toast.LENGTH_LONG);
+                }
+            }
+        });
+
+
+    }
+}
