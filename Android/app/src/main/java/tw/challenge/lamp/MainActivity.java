c08a38e4a6cb3fa3ff12af58cef6223454360319
Marek Pagel <pagel.marek@gmail.com>
2015-02-21 15:12:39 +0200

    initial scan page

--- Android/app/src/main/java/tw/challenge/lamp/MainActivity.java
+++ Android/app/src/main/java/tw/challenge/lamp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -18,8 +19,11 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Payment pay = new Payment();
         pay.execute();
-        launchScanner();
 
+
+    }
+    public void scanInitial(View v){
+        launchScanner();
     }
     public void launchScanner() {
         if (isCameraAvailable()) {
